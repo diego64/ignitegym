3ccb5048fc533724b73780aaa8e6b843f9c4608c
diego64 <diegoferreira1964@gmail.com>
2023-09-07 14:01:36 -0300

    Desenvolvimento das telas de SignIn e SignUp

--- App.tsx
+++ App.tsx
@@ -1,18 +1,24 @@
-import { Text, View, StatusBar } from 'react-native';
+import { View, StatusBar } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { THEME } from './src/theme';
+
+import { Loading } from '@components/Loading';
+//import { SignIn } from '@screens/SignIn';
+import { SignUp } from '@screens/SignUp';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: "#202024" }}>
+   <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Text>APP Ignite GYM</Text> : <View />}
-    </View>
+      {fontsLoaded ? <SignUp /> : <Loading />}
+    </NativeBaseProvider>
   );
 }
-
--- babel.config.js
+++ babel.config.js
@@ -17,10 +17,11 @@ module.exports = function(api) {
             '@services': './src/services',
             '@hooks': './src/hooks',
             '@contexts': './src/contexts',
-            '@routes': './src/routes'
+            '@routes': './src/routes',
+            '@theme': './src/theme'
           }
         },
       ],
-    ],    
+    ],
   };
 };
