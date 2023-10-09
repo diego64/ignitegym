bdd32388ad8325a86216f2f67e9c4b09e71307d7
diego64 <diegoferreira1964@gmail.com>
2023-10-09 15:58:23 -0300

    Desenvolvimento das rotas publicas

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,7 @@
+import { useNavigation } from "@react-navigation/native";
 import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+
+import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
@@ -7,53 +10,61 @@ import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
 export function SignIn() {
+  const navigation = useNavigation<AuthNavigatorRoutesProps>();
+
+  function handleNewAccount(){
+    navigation.navigate('signUp')
+  }
+
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
-    <VStack flex={1} bg="gray.700" px={10} pb={16}>
-    <Image 
-      source={BackgroundImg}
-      alt="Pessoas treinando"
-      resizeMode="contain"
-      position="absolute"
-    />
-    <Center my={24}>
-          <LogoSvg />
-
-          <Text color="gray.100" fontSize="sm">
-            Treine sua mente e o seu corpo.
-          </Text>
-        </Center>
-
-        <Center>
-          <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
-            Acesse a conta
-          </Heading>
-
-          <Input 
-            placeholder="E-mail" 
-            keyboardType="email-address"
-            autoCapitalize="none"
-
-          />
-          <Input 
-            placeholder="Senha" 
-            secureTextEntry
-          />
-
-          <Button title="Acessar" />
-        </Center>
-
-        <Center mt={24}>
-          <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
-            Ainda não tem acesso?
-          </Text>
-        </Center>
-
-        <Button 
-          title="Criar Conta" 
-          variant="outline" 
+      <VStack flex={1} bg="gray.700" px={10} pb={16}>
+        <Image 
+          source={BackgroundImg}
+          defaultSource={BackgroundImg}
+          alt="Pessoas treinando"
+          resizeMode="contain"
+          position="absolute"
         />
-      </VStack>
+          <Center my={24}>
+            <LogoSvg />
+
+            <Text color="gray.100" fontSize="sm">
+              Treine sua mente e o seu corpo.
+            </Text>
+          </Center>
+
+          <Center>
+            <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+              Acesse a conta
+            </Heading>
+
+            <Input 
+              placeholder="E-mail" 
+              keyboardType="email-address"
+              autoCapitalize="none"
+
+            />
+            <Input 
+              placeholder="Senha" 
+              secureTextEntry
+            />
+
+            <Button title="Acessar" />
+          </Center>
+
+          <Center mt={24}>
+            <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
+              Ainda não tem acesso?
+            </Text>
+          </Center>
+
+          <Button 
+            title="Criar Conta" 
+            variant="outline"
+            onPress={handleNewAccount}
+          />
+        </VStack>
     </ScrollView>
   );
 }
