d96ef04b2745fe96a2072c6b07aaf6d9a0f1da33
diego64 <diegoferreira1964@gmail.com>
2023-10-16 20:21:30 -0300

    Desenvolvimento de interfaces

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -8,6 +8,7 @@ import { ExerciseCard } from '@components/ExerciseCard';
 
 export function Home() {
   const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Trícipes', 'Ombro']);
+  const [exercises, setExercises] = useState(['Puxada frontal', 'Remada curvada', 'Ramada unilateral', 'Levantamento terras']);
   const [groupSelected, setGroupSelected] = useState('costas');
 
   return (
@@ -38,12 +39,19 @@ export function Home() {
           </Heading>
 
           <Text color="gray.200" fontSize="sm">
-            4
+            {exercises.length}
           </Text>
         </HStack>
 
-        <ExerciseCard />
-        <ExerciseCard />
+        <FlatList 
+          data={exercises}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <ExerciseCard />
+          )}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ paddingBottom: 20 }}
+        />
       </VStack>
     </VStack>
   )
